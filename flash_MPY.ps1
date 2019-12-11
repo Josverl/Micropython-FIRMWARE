20e1fd0d23c8f3934b59899f2afe890cb4fd733b
Jos Verlinde <jos_verlinde@hotmail.com>
2019-12-12 00:08:37 +0100

    update to use nighly build and new naming conventions

--- flash_MPY.ps1
+++ flash_MPY.ps1
@@ -1,3 +1,4 @@
+#Requires -Version 6
 param (
 
     [string]$port ,
@@ -6,21 +7,33 @@ param (
     
     [switch]$NoSpiram,
 
-    $BaudRate = "460800",
+    [ValidateSet("115200","256000","512000","460800","750000","921600")]    
+    $BaudRate = "750000",
     [switch]$KeepFlash,
     [ValidateSet('hard_reset','soft_reset','no_reset')]
     $after_reset = 'hard_reset',
 
-    $Path = 'C:\develop\MyPython\FIRMWARE\'
-)
+    $Path = $PSScriptRoot,
 
-Import-Module .\get-serialport.ps1
+    [switch]$nightly
+
+)
+# if ($false){
+#     Import-Module .\get-serialport.ps1 -Force
+#     $version = "v1.11"
+#     $path = "C:\develop\MyPython\FIRMWARE"
+# }
+Import-Module $PSScriptRoot\get-serialport.ps1 -Force
 
 if ([string]::IsNullOrEmpty($port))
 {
     # Select the first port 
     #Get-SerialPorts | where Service -in ('silabser','CH341SER_A64')
     $port =  (Get-SerialPort | where Service -ine 'BTHMODEM' | select -First 1).Port
+}
+if ( [string]::IsNullOrEmpty($port) ){
+    Write-error "No active port or likely serial device could be detected" 
+    exit(-1)   
 }
 Write-host -f Green "Using port $port"
 
@@ -32,11 +45,13 @@ if ($NoSpiram ){
     $spiram = "spiram"
 }
 
-#full version
-$fwname = "esp32{0}-*-{1}*.bin" -f $spiram, $version
-
-$file = Get-ChildItem -Path (join-path $path "ESP32_Micropython" $fwname) | sort | select -First 1
-
+# new naming convention :   esp32-idf3-20190529-v1.11.bin
+#                           esp32spiram-idf3-20190529-v1.11.bin
+#                           esp32spiram-idf3-20191211-v1.11-633-gb310930db.bin
+if ($nightly) { $latest = '-*' } else { $latest = '' }
+    
+$fwname = "esp32{0}-{1}-*-{2}{3}.bin" -f $spiram,'idf3',  $version , $latest
+$file = Get-ChildItem -Path (join-path -path $path -childpath "ESP32_Micropython" -AdditionalChildPath $fwname) | sort | select -First 1
 
 if ($file){
     Write-host -f Green "Found firmware" $file.Name
@@ -50,8 +65,14 @@ if ($file){
 
     Write-Host -f Green "LoadingFirmware $version $spiram from $($file.Name)"
     # program the firmware starting at address 0x1000: 
-    esptool.py --chip esp32 --port $port --baud $BaudRate write_flash -z 0x1000 $file.Name 
 
+    # TODO: check if esptool is install / can be started and 
+    try {
+        esptool.py --chip esp32 --port $port --baud $BaudRate --before default_reset --after $after_reset  write_flash --compress --flash_freq 80m --flash_size detect 0x1000  $file.Name
+        #esptool.py --chip esp32 --port $port --baud $BaudRate write_flash -z 0x1000 $file.Name 
+    } catch {
+        Write-Warning "is esptool installed ? try running  > pip install espool"
+    }
     
     cd $Savedir
 } else {
