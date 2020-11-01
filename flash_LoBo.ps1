e87eaa851f2d7446a5c35937f1997d40dcda25bf
Jos Verlinde <josverl@microsoft.com>
2020-11-01 14:34:39 +0100

    script updates

--- flash_LoBo.ps1
+++ flash_LoBo.ps1
@@ -13,7 +13,8 @@ param (
     $Path = 'C:\develop\MyPython\FIRMWARE\'
 )
 
-Import-Module .\get-serialport.ps1
+Import-Module $PSScriptRoot\get-serialport.ps1 -Force
+
 if ([string]::IsNullOrEmpty($port))
 {
     # Select the first port 
@@ -35,14 +36,14 @@ if ($folder){
     # Erase
     if(!$KeepFlash){
         Write-Host -f Green "Erasing Flash"
-        esptool.py --chip esp32 --port $port erase_flash
+        esptool --chip esp32 --port $port erase_flash
     }
     
     cd $folder.FullName
     Write-Host -f Green "LoadingFirmware $version $prebuilt from $($folder.FullName)"
     # From then on program the firmware starting at address 0x1000: 
     
-    esptool.py --chip esp32 --port $port --baud $BaudRate --before default_reset --after $after_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader/bootloader.bin 0xf000 phy_init_data.bin 0x10000 MicroPython.bin 0x8000 partitions_mpy.bin
+    esptool --chip esp32 --port $port --baud $BaudRate --before default_reset --after $after_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader/bootloader.bin 0xf000 phy_init_data.bin 0x10000 MicroPython.bin 0x8000 partitions_mpy.bin
     
     cd $Savedir
 } else {
--- flash_MPY.ps1
+++ flash_MPY.ps1
@@ -1,19 +1,22 @@
-#Requires -Version 6
+#Requires -Version 5
 param (
 
     [string]$port ,
-    [ValidateSet("v1.9.4","v1.10","v1.11")]
-    $version = "v1.11"  ,
+
+    [ValidateSet("v1.9.4","v1.10","v1.11","v1.12","v1.13","custom")]
+    $version = "v1.13"  ,
     
     [switch]$NoSpiram,
 
     [ValidateSet("115200","256000","512000","460800","750000","921600")]    
-    $BaudRate = "750000",
+    $BaudRate = "921600",
     [switch]$KeepFlash,
     [ValidateSet('hard_reset','soft_reset','no_reset')]
     $after_reset = 'hard_reset',
 
     $Path = $PSScriptRoot,
+
+    $firmware  = $null,
 
     [switch]$nightly
 
@@ -35,7 +38,7 @@ if ( [string]::IsNullOrEmpty($port) ){
     Write-error "No active port or likely serial device could be detected" 
     exit(-1)   
 }
-Write-host -f Green "Using port $port"
+Write-host -f Green "Using port ${port}:"
 
 $Savedir = $PWD
 
@@ -45,33 +48,51 @@ if ($NoSpiram ){
     $spiram = "spiram"
 }
 
-# new naming convention :   esp32-idf3-20190529-v1.11.bin
-#                           esp32spiram-idf3-20190529-v1.11.bin
-#                           esp32spiram-idf3-20191211-v1.11-633-gb310930db.bin
-if ($nightly) { $latest = '-*' } else { $latest = '' }
-    
-$fwname = "esp32{0}-{1}-*-{2}{3}.bin" -f $spiram,'idf3',  $version , $latest
-$file = Get-ChildItem -Path (join-path -path $path -childpath "ESP32_Micropython" -AdditionalChildPath $fwname) | sort | select -First 1
+# Continuous reboots after programming: Ensure FLASH_MODE is correct for your
+# board (e.g. ESP-WROOM-32 should be DIO).
+
+if ($firmware -eq $null) {
+    # new naming convention :   esp32-idf3-20190529-v1.11.bin
+    #                           esp32spiram-idf3-20190529-v1.11.bin
+    #                           esp32spiram-idf3-20191211-v1.11-633-gb310930db.bin
+    if ($nightly) { $latest = '-*' } else { $latest = '' }
+        
+    $fwname = "esp32{0}-{1}-*-{2}{3}.bin" -f $spiram,'idf3',  $version , $latest
+    $file = Get-ChildItem -Path (join-path -path $path -childpath ("ESP32_Micropython/" + $fwname) ) | sort | select -First 1
+
+    if ($version -ieq "custom"){
+        # custom fware in this folder 
+        $fwname = 'mpy_esp32_josv_spiram.bin'
+        $fwname = 'mpy_*.bin'
+        $p = join-path -path $PSScriptRoot -childpath "custom"  -AdditionalChildPath $fwname
+        Write-Host "Path : $p"
+        $file = Get-ChildItem -Path $p | sort | select -First 1
+    }
+} else {
+    # get the most recent firmware that matches the path 
+    $file = Get-ChildItem -Path $firmware | sort LastWriteTime | select -First 1
+}
+
 
 if ($file){
     Write-host -f Green "Found firmware" $file.Name
     # Erase
     if(!$KeepFlash){
         Write-Host -f Green "Erasing Flash"
-        esptool.py --chip esp32 --port $port erase_flash
+        esptool --chip esp32 --port $port erase_flash
     }
     
     cd $file.Directory
 
-    Write-Host -f Green "LoadingFirmware $version $spiram from $($file.Name)"
+    Write-Host -f Green "LoadingFirmware $version $spiram from $($file.Name) to device op port: $port"
     # program the firmware starting at address 0x1000: 
 
     # TODO: check if esptool is install / can be started and 
     try {
-        esptool.py --chip esp32 --port $port --baud $BaudRate --before default_reset --after $after_reset  write_flash --compress --flash_freq 80m --flash_size detect 0x1000  $file.Name
+        esptool --chip esp32 --port $port --baud $BaudRate --before default_reset --after $after_reset  write_flash --compress --flash_freq 80m --flash_size detect 0x1000  $file.Name
         #esptool.py --chip esp32 --port $port --baud $BaudRate write_flash -z 0x1000 $file.Name 
     } catch {
-        Write-Warning "is esptool installed ? try running  > pip install espool"
+        Write-Warning "is esptool installed ? try running  > pip install esptool"
     }
     
     cd $Savedir
